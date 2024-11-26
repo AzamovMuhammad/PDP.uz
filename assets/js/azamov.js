a58a34e324765f23bd8d9323c79cf9971cf558a6
AzamovMuhammad <azamov.dragon@gmail.com>
2024-11-26 20:17:29 +0500

    azamov.js ichiga yangi' o'zgarishlar kiritdim

--- assets/js/azamov.js
+++ assets/js/azamov.js
@@ -6,11 +6,16 @@ window.onscroll = function () {
     document.getElementById('link1').style. color = 'white';
     document.getElementById('link2').style. color = 'white';
     document.getElementById('link3').style. color = 'white';
+    document.getElementById('white').style.display = 'block'
+    document.getElementById('black').style.display = 'none'
+
   } else {
     document.getElementById("navbar").style.background = "white";
     document.getElementById('link1').style.color = 'black'
     document.getElementById('link2').style.color = 'black'
     document.getElementById('link3').style.color = 'black'
+    document.getElementById('black').style.display = 'block'
+    document.getElementById('white').style.display = 'none'
   }
 
   prevScrollpos = currentScrollpos;
