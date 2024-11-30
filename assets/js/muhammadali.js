3b9f16cc0e692eca652c96e267156f9ba2bf05ec
AzamovMuhammad <azamov.dragon@gmail.com>
2024-11-30 23:49:37 +0500

    muhammadalining ishi 100% yakunlandi.

--- assets/js/muhammadali.js
+++ assets/js/muhammadali.js
@@ -1,5 +1,3 @@
-
-
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const openModalBtn = document.querySelector(".btn-open");
@@ -29,5 +27,36 @@ const openModal = function () {
 };
 // open modal event
 openModalBtn.addEventListener("click", openModal);
+
+
+const modal2 = document.querySelector(".modal2");
+const overlay2 = document.querySelector(".overlay2");
+const openModalBtn2 = document.querySelector(".btn-open2");
+const closeModalBtn2 = document.querySelector(".btn-close2");
+
+// close modal function
+const closeModal2 = function () {
+  modal2.classList.add("hidden2");
+  overlay2.classList.add("hidden2");
+};
+
+// close the modal when the close button and overlay is clicked
+closeModalBtn2.addEventListener("click", closeModal2);
+overlay2.addEventListener("click", closeModal2);
+
+// close modal when the Esc key is pressed
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal2.classList.contains("hidden2")) {
+    closeModal2();
+  }
+});
+
+// open modal function
+const openModal2 = function () {
+  modal2.classList.remove("hidden2");
+  overlay2.classList.remove("hidden2");
+};
+// open modal event
+openModalBtn2.addEventListener("click", openModal2);
 
   
